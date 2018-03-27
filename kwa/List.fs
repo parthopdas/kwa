8fcfd58f561e51b18487a3f46ab38b667fc94af8
Partho P. Das <parthopdas@live.com>
2018-03-27 23:55:14 +1000

    Working

---
+++ kwa/List.fs
@@ -0,0 +1,61 @@
+﻿module Kwa.List
+
+let traverseAsyncA f list = 
+    let (<*>) = Async.apply
+    let retn = Async.result
+
+    let cons head tail = head :: tail
+    let initState = retn []
+
+    let folder head tail = 
+        retn cons <*> (f head) <*> tail
+
+    List.foldBack folder list initState
+
+let sequenceAsyncA x = traverseAsyncA id x
+
+let traverseAsyncM f list =
+    let (>>=) x f = Async.bind f x
+    let retn = Async.result
+
+    let cons head tail = head :: tail
+
+    let initState = retn []
+    let folder head tail = 
+        f head >>= (fun h -> 
+        tail >>= (fun t ->
+        retn (cons h t) ))
+
+    List.foldBack folder list initState
+
+let sequenceAsyncM x = traverseAsyncM id x
+
+let traverseResultA f list = 
+    let (<*>) = Result.apply
+    let retn = Result.Ok
+
+    let cons head tail = head :: tail
+    let initState = retn []
+
+    let folder head tail = 
+        retn cons <*> (f head) <*> tail
+
+    List.foldBack folder list initState
+
+let sequenceResultA x = traverseResultA id x
+
+let traverseResultM f list =
+    let (>>=) x f = Result.bind f x
+    let retn = Result.Ok
+
+    let cons head tail = head :: tail
+
+    let initState = retn []
+    let folder head tail = 
+        f head >>= (fun h -> 
+        tail >>= (fun t ->
+        retn (cons h t) ))
+
+    List.foldBack folder list initState
+
+let sequenceResultM x = traverseResultM id x
---
+++ kwa/Program.fs
@@ -0,0 +1,84 @@
+﻿open System
+open System.Net
+open HtmlAgilityPack
+open Kwa
+
+[<Struct>]
+type Article =
+    { Title: string
+      Url: Uri
+      Date: DateTime
+      Tags: string list }
+
+type CsvRecord = 
+    | Article of Article
+    | ArticleError of string
+
+    static member toCsvLine = function
+        | Article a -> 
+            let tags = 
+                if (List.isEmpty a.Tags) then
+                    [ String.Empty ]
+                else 
+                    a.Tags
+            tags |> List.map (sprintf "%O,%O,%O,%O" a.Date (a.Title.Trim().Replace(",", "|")) a.Url)
+        | ArticleError s -> [ s ]
+
+let getUrlForPage =
+    sprintf "http://blog.kaggle.com/category/winners-interviews/page/%d/"
+    >> Uri
+
+let getPage (uri: Uri) = 
+    async {
+        use client = new WebClient()
+        try
+            printfn " [%O] Started ..." uri
+            let! html = uri |> client.DownloadStringTaskAsync |> Async.AwaitTask
+            printfn " [%O] ... finished" uri
+            return Ok html
+        with
+        | ex -> 
+            printfn " [%O] ... exception" ex.Message
+            let err = sprintf "[%s] %A" uri.Host ex.Message
+            return Error err
+    }
+
+let extractArticleFromNode (node: HtmlNode): Result<Article, string> = 
+    try 
+        let entryTitle = node.SelectSingleNode(".//*[@class='entry-title']")
+        let tags = 
+            node.SelectNodes(".//footer/a")
+            |> Option.ofNull
+            |> Option.getOrElse (HtmlNodeCollection(node))
+            |> Seq.map (fun a -> a.InnerText)
+            |> Seq.toList
+        let url = entryTitle.SelectSingleNode("a").Attributes.["href"].Value |> Uri
+        let date = node.SelectSingleNode(".//*[@class='entry-date']").Attributes.["datetime"].Value |> DateTime.Parse
+
+        { Title = entryTitle.InnerText
+          Url = url
+          Date = date
+          Tags = tags } |> Ok
+    with e -> Error e.Message
+
+let extractArticlesFromPage (page: string): Result<Article, string> list =
+    let doc = HtmlDocument()
+    doc.LoadHtml(page)
+    doc.DocumentNode.SelectNodes("//article")
+    |> Seq.map extractArticleFromNode
+    |> Seq.toList
+
+[<EntryPoint>]
+let main argv =
+    [1..19]
+    |> List.map (getUrlForPage >> getPage)
+    |> List.sequenceAsyncA
+    |> Async.map List.sequenceResultA
+    |> Async.map (Result.map (List.map extractArticlesFromPage))
+    |> Async.RunSynchronously
+    |> Result.map (List.collect id)
+    |> Result.fold (List.map <| Result.fold Article ArticleError) (ArticleError >> List.singleton)
+    |> List.collect CsvRecord.toCsvLine
+    |> List.iter (printf "%s\r\n")
+
+    0
